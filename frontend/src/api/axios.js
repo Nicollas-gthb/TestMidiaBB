708f5fde341ab89752146669b96f4902d1de12f3
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-04-04 01:19:20 -0300

    feat: axios

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+export const api = axios.create({
+    baseURL: "/api"
+})
+
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token")
+
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config
+    }, 
+    error => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if(error.response && error.response.status === 401){
+            localStorage.removeItem("token")
+        }
+
+        return Promise.reject(error)
+    }
+)
