61b371bbd075a807ca46840793f67ae585f1e482
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-04-04 15:01:16 -0300

    feat: bootstrap instalado

--- frontend/src/pages/midias/Midia.jsx
+++ frontend/src/pages/midias/Midia.jsx
@@ -20,62 +20,36 @@ export default function Midia() {
             <main id="home-main-container">
 
                 {/** TODO: converter o header em um componente */}
-                <header id="home-menu-header">
+                <header id="midia-menu-header">
                     <h1>LOGO</h1>
                 </header>
 
-                <div id="home-menu-main">
-                    <h1>Gestão de midias</h1>
+                <div id="midia-menu-main">
 
-                    <button>Nova midia</button>
+                    <div className="table-title">
 
-                    <p>Lista de tvs</p>
+                        <h2>Gestão de midias</h2>
+        
+                        <button className="action-button">Nova midia</button>
+
+                    </div>
 
                     <div className="table-container">
                         <table>
                             <thead>
                                 <tr>
-                                    <th>ID</th>
+                                    <th className="left-table">ID</th>
                                     <th>NOME</th>
                                     <th>TIPO</th>
                                     <th>STATUS</th>
                                     <th>TVS ASSOCIADAS</th>
                                     <th>VALIDADE</th>
                                     <th>URL</th>
-                                    <th>AÇÕES</th>
+                                    <th className="right-table">AÇÕES</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                </tr>
-                                <tr>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                </tr>
-                                <tr>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                    <td>a</td>
-                                </tr>
+                                {}
                             </tbody>
                         </table>
                     </div>
