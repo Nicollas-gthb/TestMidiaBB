7d55e4e866ee8212084117f25fac2d3caa572738
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-05-13 20:03:18 -0300

    feat: add novos icones no aside

--- frontend/src/components/aside/Aside.jsx
+++ frontend/src/components/aside/Aside.jsx
@@ -16,82 +16,135 @@ export const Aside = () => {
             <header id="aside-header">
                 <h1>Menu</h1>
             </header>
+
             <div id="aside-main">
+                
                 <button
-                    className={`menu-buttons ${isActive("/home") ? "menu-active" : "menu-inactive"}`}
+                    className={`menu-buttons processo ${isActive("/home") ? "menu-active" : "menu-inactive"}`}
                     onClick={() => navigate("/home")}
                 >
                     <i className={`bi aside-bi bi-house`}></i>
                     Home
                 </button>
-                <button
-                    className={`menu-buttons ${isActive("/midia") ? "menu-active" : "menu-inactive"}`}
-                    onClick={() => navigate("/midia")}
-                >
-                    <i className="bi aside-bi bi-image"></i>
-                    Mídias
-                </button>
-                <button
-                    className={`menu-buttons ${isActive("/tv") ? "menu-active" : "menu-inactive"}`}
-                    onClick={() => navigate("/tv")}
-                >
-                    <i className="bi aside-bi bi-tv"></i>
-                    TVs
-                </button>
 
-                <hr />
+                <div className="nav-group">
+                    <p className="nav-group-title">Midias</p>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/midia")}
+                    >
+                        <i className="bi aside-bi bi-image"></i>
+                        M. Ativas
+                    </button>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/midia")}
+                    >
+                        <i className="bi aside-bi bi-calendar-event"></i>
+                        M. Agendadas
+                    </button>
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/midia")}
+                    >
+                        <i className="bi aside-bi bi-clock"></i>
+                        M. Expiradas
+                    </button>
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/midia")}
+                    >
+                        <i className="bi aside-bi bi-list-ul"></i>
+                        Todas as Midias
+                    </button>
+
+                </div>
+
+                <div className="nav-group">
+
+                    <p className="nav-group-title">TVs</p>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/tv")}
+                    >
+                        <i className="bi aside-bi bi-tv"></i>
+                        TVs Ativas
+                    </button>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/tv")}
+                    >
+                        <i className="bi aside-bi bi-wifi-off"></i>
+                        TVs Offline
+                    </button>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                        onClick={() => navigate("/tv")}
+                    >
+                        <i className="bi aside-bi bi-list-ul"></i>
+                        Todas as TVS
+                    </button>
+
+                </div>
+
+
+                <div className="nav-group">
+
+                    <p className="nav-group-title">Relatórios</p>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                    >
+                        <i className="bi aside-bi bi-card-heading"></i>
+                        Log Atividades
+                    </button>
+                    
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                    >
+                        <i className="bi aside-bi bi-file-earmark-bar-graph"></i>
+                        Relatórios
+                    </button>
+
+                </div>
+
+
+                <div className="nav-group">
+
+                    <p className="nav-group-title">Configurações</p>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                    >
+                        <i className="bi aside-bi bi-person"></i>
+                        Usuários
+                    </button>
+                    
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                    >
+                        <i className="bi aside-bi bi-shield-lock"></i>
+                        Auths
+                    </button>
+
+                    <button
+                        className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
+                    >
+                        <i className="bi aside-bi bi-gear"></i>
+                        Configs
+                    </button>
+                </div>
 
                 
-                    <p className="processo">em processo</p>
-
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    <i className="bi aside-bi bi-wifi-off"></i>
-                    TVs Offline
-                </button>
                 
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    <i className="bi aside-bi bi-card-heading"></i>
-                    Log de Atividades
-                </button>
                 
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    <i className="bi aside-bi bi-file-earmark-bar-graph"></i>
-                    Relatórios
-                </button>
 
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    <i className="bi aside-bi bi-person"></i>
-                    Usuários
-                </button>
-                
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    {/* <i ="bi  bi-gear"></i> */}
-                    <i className="bi aside-bi bi-shield-lock"></i>
-                    Auths
-                </button>
-
-                <button
-                    className={`menu-buttons processo ${isActive("/teste") ? "menu-active" : "menu-inactive"}`}
-                    
-                >
-                    <i className="bi aside-bi bi-gear"></i>
-                    Configs
-                </button>
+            
 
                 
             </div>
