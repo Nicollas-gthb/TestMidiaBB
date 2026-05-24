45040abb3b4a7c4964d982626fca30ad34c764f6
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-05-23 21:11:35 -0300

    fix: restruturação da pagina de graficos e historico

--- backend/app/routes/dashboard.py
+++ backend/app/routes/dashboard.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import func
+from datetime import date, datetime, timezone
 
 from app.core.database import get_session
 from app.models.midia import Midia
@@ -41,21 +42,83 @@ def tv_midias(
     session: Session = Depends(get_session)
 ):
 
-    result = (
-        session.query(
-            TV.nome,
-            func.count(PlaylistItem.id)
+    tvs = session.query(TV).all()
+
+    resultado = []
+
+    for tv in tvs:
+
+        total = (
+            session.query(PlaylistItem)
+            .filter(PlaylistItem.tv_id == tv.id)
+            .count()
         )
-        .join(PlaylistItem, PlaylistItem.tv_id == TV.id)
-        .group_by(TV.nome)
-        .all()
-    )
+
+        ativas = (
+            session.query(PlaylistItem)
+            .join(Midia)
+            .filter(
+                PlaylistItem.tv_id == tv.id,
+                Midia.ativo == True
+            )
+            .count()
+        )
+
+        resultado.append({
+            "tv": tv.nome,
+            "total": total,
+            "ativas": ativas,
+            "status": tv.ativo
+        })
+
+    return resultado
+
+@router.get("/media-status")
+def media_status(
+    session: Session = Depends(get_session)
+):
+
+    agora = datetime.now(timezone.utc)
+
+    ativas = session.query(Midia).filter(
+        Midia.ativo == True,
+        Midia.inicio_exibicao < agora,
+        Midia.expiracao > agora
+    ).count()
+
+    expiradas = session.query(Midia).filter(
+        Midia.ativo == True,
+        Midia.expiracao < agora
+    ).count()
+
+    agendadas = session.query(Midia).filter(
+        Midia.ativo == True,
+        Midia.inicio_exibicao > agora
+    ).count()
+
+    removidas = session.query(Midia).filter(
+        Midia.ativo == False
+    ).count()
 
     return [
         {
-            "tv": nome,
-            "midias": total
+            "status": "Ativas",
+            "valor": ativas,
+            "fill": "#16ee48"
+        },
+        {
+            "status": "Agendadas",
+            "valor": agendadas,
+            "fill": "#00ddff"
+        },
+        {
+            "status": "Expiradas",
+            "valor": expiradas,
+            "fill": "#ffbf00"
+        },
+        {
+            "status": "Removidas",
+            "valor": removidas,
+            "fill": "#ff0019"
         }
-        for nome, total in result
     ]
-
--- frontend/src/pages/reports/Reports.jsx
+++ frontend/src/pages/reports/Reports.jsx
@@ -1,7 +1,7 @@
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid} from "recharts"
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts" // grafico de pizza
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid} from "recharts" // grafico de barras
 
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 
 import "./Reports.css"
 import { api } from "../../api/axios"
@@ -9,6 +9,7 @@ import { useToast } from "../../contexts/ToastContext"
 import { formatarDataHora } from "../../utils/formatters"
 import { Aside } from "../../components/aside/Aside"
 import { Header } from "../../components/header/Header"
+import { ThemeContext } from "../../contexts/ThemeContext"
 
 export default function Reports(){
 
@@ -19,13 +20,16 @@ export default function Reports(){
         "#656970"
     ]
 
+    const { theme } = useContext(ThemeContext)
+    const axisTextColor = theme === "light" ? "#000000" : "#dfdfdf"
+
     const { addToast } = useToast()
-    
     const [historico, setHistorico] = useState([])
 
     const [mediaTypes, setMediaTypes] = useState([])
     const [totalMidias, setTotalMidias] = useState(0)
-    //const [tvStats, setTvStats] = useState([])
+    const [tvStats, setTvStats] = useState([])
+    const [mediaStatus, setMediaStatus] = useState([])
 
     useEffect(() => {
         api.get("/dashboard/media-types")
@@ -35,16 +39,22 @@ export default function Reports(){
             })
     }, [])
 
-    // useEffect(() => {
-    //     api.get("/dashboard/tv-midias")
-    //         .then(res => {
+    useEffect(() => {
+        api.get("/dashboard/tv-midias")
+            .then(res => {
+                console.log(res.data)
+                setTvStats(res.data)
+            })
 
-    //             console.log(res.data)
+    }, [])
 
-    //             setTvStats(res.data)
-    //         })
-
-    // }, [])
+    useEffect(() => {
+        api.get("/dashboard/media-status")
+            .then(res => {
+                console.log(res.data)
+                setMediaStatus(res.data)
+            })
+    }, [])
 
     useEffect(() => {
         const carregarHistorico = async () => {
@@ -72,18 +82,56 @@ export default function Reports(){
                 <div id="reports-menu-main">
                     <h2>Relatórios</h2>
 
-                    <div className="reports-cards-container"></div>
+                    <div className="reports-cards-container">
+
+                        <div id="axis-container" className="reports-cards">
+                            <h2>Mídias por TV</h2>
+
+                                <ResponsiveContainer width={"100%"} height={400}>
+
+                                    <BarChart data={tvStats}>
+
+                                        <CartesianGrid strokeDasharray="3 3" />
+
+                                        <XAxis
+                                            dataKey="tv"
+                                            tick={{ fill: axisTextColor }}
+                                        />
+
+                                        <YAxis
+                                            tick={{ fill: axisTextColor }}
+                                        />
+
+                                        <Tooltip />
+
+                                        <Bar dataKey="total">
+
+                                            {tvStats.map((entry, index) => (
+
+                                                <Cell
+                                                    key={index}
+                                                    fill={entry.status ? "#22c55e" : "#ef4444"}
+                                                />
+
+                                            ))}
+
+                                        </Bar>
+
+                                        <Bar
+                                            dataKey="ativas"
+                                            fill="#3b82f6"
+                                        />
+
+                                    </BarChart>
+
+                                </ResponsiveContainer>
+                        </div>
+
+                    </div>
 
                     <div className="reports-cards-container">
 
-                        <div className="reports-cards-graficos">
-
-                            
-                            <div className="reports-cards">
-                                <div className="reports-chart-container">
-                                    
-                                </div>
-                            </div>
+                        <section className="reports-cards-graficos">
 
                             <div className="reports-cards">
                                 <div id="donut-container" className="reports-chart-container">
@@ -116,8 +164,6 @@ export default function Reports(){
                                         <p>mídias</p>
                                     </div>
 
-                                    
-
                                 </div>
 
                                 <div id="donut-legend">
@@ -128,57 +174,115 @@ export default function Reports(){
                                         </div>
                                     ))}
                                 </div>
-
                             </div>
-           
-                        </div>
+                            
+                            <div className="reports-cards">
+                                <div className="reports-chart-container">
 
-                        
-                        <div id="card-reports-historico" className="reports-cards">
-                            <div className="home-card-head">
-                                <i className="bi card-bi bi-clock"></i>
-                                Histórico Recente
+                                    <h2>Estado das Mídias</h2>
+
+                                    <ResponsiveContainer width="90%" height={"100%"}>
+
+                                        <BarChart
+                                            data={mediaStatus}
+                                            layout="vertical"
+                                        >
+
+                                            <XAxis
+                                                type="number"
+                                                tick={{ fill: axisTextColor, fontSize: 11}}
+                                            />
+
+                                            <YAxis
+                                                type="category"
+                                                dataKey="status"
+                                                tick={{ fill: axisTextColor, fontSize: 11}}
+                                            />
+
+                                            <Tooltip />
+
+                                            <Bar dataKey="valor">
+
+                                                {mediaStatus.map((entry, index) => (
+
+                                                    <Cell
+                                                        key={index}
+                                                        fill={entry.fill}
+                                                    />
+
+                                                ))}
+
+                                            </Bar>
+
+                                        </BarChart>
+
+                                    </ResponsiveContainer>
+
+                                </div>
+
+                                <div id="bar-legend">
+                                    {mediaStatus.map((entry, index) => (
+                                        <div key={index} className="bar-legend-item">
+                                            <section className="bar-legend-color" style={{ backgroundColor: entry.fill }}></section>    
+                                            <p className="bar-legend-text">{entry.status} : {entry.valor}</p>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
 
-                            <div className="home-card-body">
-                                {historico.length === 0 ? (
-                                    <div className="home-card-vazio">
-                                        <p>Sem Histórico</p>
-                                    </div>
-                                ) : (
+                        </section>
+
+                        <section className="reports-cards-graficos">
+
+                            <div id="card-reports-historico" className="reports-cards">
+
+                                <div className="home-card-head">
+                                    <i className="bi card-bi bi-clock"></i>
+                                    Histórico Recente
+                                </div>
+
+                                <div className="home-card-body">
+                                    {historico.length === 0 ? (
+                                        <div className="home-card-vazio">
+                                            <p>Sem Histórico</p>
+                                        </div>
+                                    ) : (
+                                        
+                                        <table>
+                                            <tbody>
+                                                {historico.map(h => (
+                                                    <tr key={h.id}>
+                                        
+                                                        <td id="historico-content">
+                                                            <span className={`historico-icon home-tag-${h.acao.replace(" ", "-")}`}>
+                                                                
+                                                            </span>
+                                                            <span className="historico-text">
+
+                                                                <p className="historico-main-text">{`${h.entidade} "${h.entidade_nome}" foi ${h.acao}`}</p>
+                                                                <p className="historico-sub-text">{`${h.usuario_nome}`}</p>
+                            
+                                                            </span>
+                                                        </td>
+                                                
+                                                        <td >
+                                                            <span className="historico-data">
+                                                                {formatarDataHora(h.criado_em)}
+                                                            </span>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
                                     
-                                    <table>
-                                        <tbody>
-                                            {historico.map(h => (
-                                                <tr key={h.id}>
-                                    
-                                                    <td id="historico-content">
-                                                        <span className={`historico-icon home-tag-${h.acao.replace(" ", "-")}`}>
-                                                            
-                                                        </span>
-                                                        <span className="historico-text">
-
-                                                            <p className="historico-main-text">{`${h.entidade} "${h.entidade_nome}" foi ${h.acao}`}</p>
-                                                            <p className="historico-sub-text">{`${h.usuario_nome}`}</p>
-                        
-                                                        </span>
-                                                    </td>
-                                            
-                                                    <td >
-                                                        <span className="historico-data">
-                                                            {formatarDataHora(h.criado_em)}
-                                                        </span>
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                   
-                                )}
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                        
+                            
+                        </section>
+
                     </div>
+                        
                 </div>
             </main>
         </div>
